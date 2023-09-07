def0ec3d0544f2cbbdeadd29bb61c5c64e2088f2
Xavier <sapphirus@azorium.net>
2023-09-07 19:02:02 -0400

    Fixed package summary output, and cleaned up output for erroneous transaction handling.

--- src/sync/transaction/commit.rs
+++ src/sync/transaction/commit.rs
@@ -10,7 +10,7 @@ use crate::{sync::{
     utils::format_unit, 
     dl_event::{DownloadCallback, self}}, 
     exec::utils::execute_in_container, 
-    utils::print_error};
+    utils::{print_error, print_warning}};
 
 use crate::utils::prompt::prompt;
 use crate::config::InstanceHandle;
@@ -107,38 +107,37 @@ fn keyring_update(ag: &mut TransactionAggregator, inshandle: &InstanceHandle) {
 }
 
 fn summary(handle: &Alpm) { 
-    let remove = if handle.trans_remove().len() > 0 {
-        true
-    } else {
-        false
-    };
-    let packages = if remove {
-        handle.trans_remove()
-    } else {
-        handle.trans_add()
-    };
- 
-    let size = Term::size(&Term::stdout());
     let mut installed_size_old: i64 = 0;
     let mut installed_size: i64 = 0;
     let mut download: i64 = 0;
-    let mut files_to_download: usize = 0;
+    let mut files_to_download: usize = 0; 
+    let mut pkglist: String = String::new();
+    let mut current_line_len:usize = 0;
+    let remove = if handle.trans_remove().len() > 0 { true } else { false };
+    let packages = if remove { handle.trans_remove() } else { handle.trans_add() };
+    let total_str = if remove { "Total Removed Size" } else { "Total Installed Size" };
+    let size = Term::size(&Term::stdout());
     let preface = format!("Packages ({}) ", packages.len());
-    let mut print_string: String = String::new();
-    let line_delimiter = size.1 as isize - preface.len() as isize;
-    let mut current_line_len: isize = 0;
+    let preface_newline = " ".repeat(preface.len()); 
+    let line_delimiter = size.1 as usize - preface.len();
+   
+    print!("\n{}", style(format!("{preface}")).bold());
 
-    print!("\n{}", style(format!("{}", preface)).bold());
-
-    for val in packages { 
-        let pkg_sync = val;
+    for pkg_sync in packages { 
         let pkg = match handle.localdb().pkg(pkg_sync.name()) {
-            Ok(pkg) => pkg,
-            Err(_) => pkg_sync,
+            Ok(pkg) => pkg, Err(_) => pkg_sync,
         };
         let output = format!("{}-{} ", pkg.name(), style(pkg_sync.version()).dim()); 
         let download_size = pkg_sync.download_size();
+        let string_len = pkg.name().len() + pkg_sync.version().len() + 2;
 
+        if current_line_len+string_len >= line_delimiter { 
+            println!("{pkglist}"); 
+            pkglist = preface_newline.clone();
+            current_line_len = pkglist.len(); 
+        }
+
+        current_line_len += string_len;
         installed_size_old += pkg.isize();             
         installed_size += pkg_sync.isize();
         
@@ -147,27 +146,13 @@ fn summary(handle: &Alpm) {
             files_to_download += 1;
         }
 
-        current_line_len += print_string.len() as isize;
-        print_string.push_str(&output);  
-
-        if current_line_len >= line_delimiter { 
-            print!("{}\n", print_string);
-            print_string = " ".repeat(preface.len());
-            current_line_len = 0;
-        }
+        pkglist.push_str(&output);  
     }
 
-    if print_string.len() > 0 {
-        print!("{}\n\n", print_string);
-    }
-              
+    print!("{pkglist}\n\n");
+    println!("{}: {}", style(total_str).bold(), format_unit(installed_size));  
+
     let net = installed_size-installed_size_old;
-
-    if remove {
-        println!("{}: {}", style("Total Removed Size").bold(), format_unit(installed_size));  
-    } else {
-        println!("{}: {}", style("Total Installed Size").bold(), format_unit(installed_size));  
-    }
 
     if net != 0 {
         println!("{}: {}", style("Net Upgrade Size").bold(), format_unit(net)); 
@@ -184,26 +169,27 @@ fn summary(handle: &Alpm) {
 fn erroneous_transaction<'a>(error: (CommitResult<'a>, alpm::Error)) -> Result<()> {
     match error.0 {
         CommitResult::FileConflict(file) => {
-            print_error("Conflicting files in container filesystem:");
             for conflict in file {
                 match conflict.conflict_type() {
                     FileConflictType::Filesystem => {
                         let file = conflict.file();
                         let target = conflict.target();
-                        println!("{}: '{}' already exists.", target, file);
+                        print_warning(format!("{}: '{}' already exists.", target, file));
                     },
                     FileConflictType::Target => {
                         let file = conflict.file();
                         let target = style(conflict.target()).bold().white();
                         if let Some(conflicting) = conflict.conflicting_target() { 
                             let conflicting = style(conflicting).bold().white();
-                            println!("{}: '{}' is owned by {}", target, file, conflicting); 
+                            print_warning(format!("{}: '{}' is owned by {}", target, file, conflicting)); 
                         } else {
-                            println!("{}: '{}' is owned by foreign target", target, file);
+                            print_warning(format!("{}: '{}' is owned by foreign target", target, file));
                         }
                     },
                 }
             }
+
+            Err(Error::TransactionFailure("Conflict within container filesystem".into()))?
         },
         CommitResult::PkgInvalid(p) => {
             for pkg in p.iter() {
