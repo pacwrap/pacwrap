58e955887cbfb2c084d699d353e76996ef604496
Xavier <sapphirus@azorium.net>
2023-10-04 09:45:24 -0400

    Removal of pacwrap-bash create script

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use utils::arguments::{self, Arguments};
 
+
 mod config;
 mod exec;
 mod constants;
@@ -16,7 +17,6 @@ enum Options {
     Compat,
     Interpose,
     Exec,
-    BashCreate,
     BashProc,
     BashHelp,
     BashUtils,
@@ -36,9 +36,8 @@ fn main() {
         .short("-E").long("--exec").set(Options::Exec)
         .short("-V").long("--version").set(Options::Version)
         .short("-Axc").long("--aux-compat").set(Options::Compat)
-        .short("-C").long("--create").set(Options::BashCreate)
         .short("-P").long("--proc").set(Options::BashProc)
-        .short("-h").long("--man").set(Options::BashHelp)
+        .short("-H").long("--help").set(Options::BashHelp)
         .short("-U").long("--utils").set(Options::BashUtils)
         .parse_arguments();
 
@@ -51,7 +50,6 @@ fn main() {
         Options::Compat => compat::compat(),
         Options::Version => arguments::print_version(),
         Options::BashUtils => compat::execute_bash("utils"),
-        Options::BashCreate => compat::execute_bash("create"),
         Options::BashProc => compat::execute_bash("ps"), 
         Options::BashHelp => compat::execute_bash("man"),
         Options::None => arguments::invalid(),
