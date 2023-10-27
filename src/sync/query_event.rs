da3e35d75162752c34a008eb9b118f8b756f1a64
Xavier <sapphirus@azorium.net>
2023-10-27 05:48:07 -0400

    Implemented key import and corrupted prompts

--- src/sync/query_event.rs
+++ src/sync/query_event.rs
@@ -13,23 +13,45 @@ pub fn questioncb(question: AnyQuestion, this: &mut QueryCallback) {
             let reason = x.conflict().reason();
             let pkg_a = x.conflict().package1();
             let pkg_b = x.conflict().package2();
-
-            let prompt_string = format!("Conflict between {} and {}; Remove {}?", pkg_a, pkg_b, pkg_b);
+            let prompt_string = format!("Conflict between {pkg_a} and {pkg_b}; Remove {pkg_b}?");
             let prompt =  prompt("->", prompt_string, false);
+            
             if let Ok(_) = prompt {
                 x.set_remove(true);
             }
         },
         Replace(x) => { 
-            let prompt_string = format!("Replace package {} with {}?", x.oldpkg().name(), x.newpkg().name());
+            let old = x.oldpkg().name();
+            let new = x.newpkg().name();
+            let prompt_string = format!("Replace package {old} with {new}?");
             let prompt =  prompt("->", prompt_string, false);
+            
             if let Ok(_) = prompt {
                 x.set_replace(true);
             }
         },
-        Corrupted(x) => (),
-        RemovePkgs(x) => (),
-        ImportKey(x) => (),
-        _ => ()
+        Corrupted(mut x) => {
+            let prompt_string = format!("Remove corrupted package {}?", x.filepath());
+            let prompt =  prompt("->", prompt_string, false);
+            
+            if let Ok(_) = prompt {
+                x.set_remove(true);
+            }
+        },
+        ImportKey(mut x) => {
+            let key = x.key();
+            let fingerprint = key.fingerprint();
+            let email = key.email();
+            let name = key.name();
+            let prompt_string = format!("Import key {fingerprint},\"{name} <{email}>\" to keyring?");
+            let prompt =  prompt("->", prompt_string, false);
+            
+            if let Ok(_) = prompt {
+                x.set_import(true);
+            } 
+        },
+        RemovePkgs(_) => (),
+        SelectProvider(_) => (),
+        InstallIgnorepkg(_) => (),
     }
 }
