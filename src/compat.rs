ef6cfad56247a8da75832b276500e3817b335951
Xavier <sapphirus@azorium.net>
2023-10-27 06:03:35 -0400

    Removed --save function from bash interoperability layer.
    - In addition, generate_config() in pacwrap-common is now inert.
    - Specify dependent packages in package replication arguments.

--- src/compat.rs
+++ src/compat.rs
@@ -1,85 +1,18 @@
 use std::{process::Command, env};
-use std::rc::Rc;
 
-use alpm::PackageReason;
 
-use crate::log::Logger;
-use crate::{sync, utils};
-use crate::config::{self, 
-    Instance, 
-    InsVars, 
-    InstanceType, 
-    InstanceHandle};
-use crate::utils::{print_help_error, print_error};
+use crate::config;
+use crate::utils::print_help_error;
 use crate::utils::{arguments::{Arguments, Operand}, 
-    handle_process, 
-    env_var};
+    handle_process};
 
-fn save_configuration(ins: &str) {
-    let mut logger = Logger::new("pacwrap-compat").init().unwrap();
-    let mut pkgs = Vec::new();
-    let deps: Vec<Rc<str>> = env_var("PACWRAP_DEPS").split_whitespace().map(|a| a.into()).collect(); 
-    let ctype = InstanceType::new(env_var("PACWRAP_TYPE").as_str()); 
-    let mut instance = match config::provide_handle(ins) {
-        Ok(handle) => handle,
-        Err(_) => {
-            let vars = InsVars::new(ins);
-            let cfg = Instance::new(ctype, pkgs.clone(), deps.clone());
-            InstanceHandle::new(cfg, vars)
-        }
-    };
 
-    let depends = instance.metadata().dependencies();
-    let dep_depth = depends.len();
-    let alpm =  sync::instantiate_alpm(&instance);
-    let mut skip = Vec::new();
-
-    if dep_depth > 0 {
-        let dep = &depends[dep_depth-1];
-
-        match config::provide_handle(dep) {
-            Ok(dep_handle) => {
-                let alpm =  sync::instantiate_alpm(&dep_handle);
-
-                for pkg in alpm.localdb()
-                    .pkgs()
-                    .iter()
-                    .filter(|p| p.reason() == PackageReason::Explicit)
-                    .collect::<Vec<_>>() {
-                    skip.push(pkg.name().to_string());
-                }
-            },
-            Err(error) => {
-                print_error(error);
-                std::process::exit(1);
-            }
-        }
-    }
-
-    for pkg in alpm.localdb()
-        .pkgs()
-        .iter()
-        .filter(|p| p.reason() == PackageReason::Explicit 
-            && ! skip
-                .contains(&p.name()
-                    .into()))
-        .collect::<Vec<_>>() {
-        pkgs.push(pkg.name().into());
-    }
-
-    alpm.release().unwrap();
-    instance.metadata_mut().set(deps, pkgs);
-    config::save_handle(&instance).ok(); 
-    logger.log(format!("configuration file written for {ins} via compatibility layer")).unwrap();
+fn save_configuration() -> Result<(), String> {
+    Err("This functionality has been deprecated and removed.")?
 }
 
-fn print_configuration(instance: &str) {
-    let ins = match config::provide_handle(instance) {
-        Ok(ins) => ins, Err(error) => { 
-            utils::print_error(error);
-            std::process::exit(1);
-        },
-    };
+fn print_configuration(instance: &str) -> Result<(),String> {
+    let ins = config::provide_new_handle(instance)?;
     let depends = ins.metadata().dependencies();
     let pkgs = ins.metadata().explicit_packages();
     let mut pkgs_string = String::new();
@@ -95,14 +28,20 @@ fn print_configuration(instance: &str) {
     for i in pkgs.iter() {
         pkgs_string.push_str(&format!("{} ", i));
     }
+
     println!("INSTANCE_CONFIG[{},3]=\"{}\"", instance, pkgs_string);
+    Ok(())
 }
 
 pub fn compat(mut args: Arguments) {
-    match args.next().unwrap_or_default() {
-        Operand::Short('s') | Operand::Long("save") => save_configuration(args.target()),
-        Operand::Short('l') | Operand::Long("load") => print_configuration(args.target()), 
-        _ => print_help_error(args.invalid_operand())
+    let result = match args.next().unwrap_or_default() {
+        Operand::Short('s') | Operand::Long("save") => save_configuration(),
+        Operand::Short('l') | Operand::Long("load") => print_configuration(args.target()),
+        _ => Err(args.invalid_operand())
+    };
+
+    if let Err(error) = result {
+        print_help_error(error);
     }
 }
 
