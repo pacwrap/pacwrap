d35dd95be36e674538548546821851b03219c195
Xavier <sapphirus@azorium.net>
2023-10-30 09:27:07 -0400

    Obtain timestamp from git on release target for reproducible builds.

--- build.rs
+++ build.rs
@@ -8,16 +8,20 @@ fn head() -> String {
     }  
 }
 
-fn time() -> String {
-    match Command::new("date").arg("+%d-%m-%Y").output() {
-        Ok(output) =>  String::from_utf8(output.stdout).unwrap_or("N/A".into()),
-        Err(_) => "N/A".into(),
-    }  
+fn time(debug: bool) -> String {
+    match debug {
+        false => match Command::new("git").args(["log", "-1", "--date=format:%d/%m/%Y", "--format=%ad"]).output() {
+            Ok(output) =>  String::from_utf8(output.stdout).unwrap_or("N/A".into()),
+            Err(_) => "N/A".into(),
+        },
+        true => match Command::new("date").args(["+%d/%m/%Y %T"]).output() {
+            Ok(output) =>  String::from_utf8(output.stdout).unwrap_or("N/A".into()),
+            Err(_) => "N/A".into(),
+        }
+    }
 }
 
-fn release() -> &'static str {
-    let debug: bool = var("DEBUG").unwrap().parse().unwrap();
-
+fn release(debug: bool) -> &'static str {
     match debug {
         true => "DEV", false => "RELEASE",
     }
@@ -30,10 +34,15 @@ fn dist_repo() -> String {
     }
 }
 
+fn is_debug() -> bool {
+    var("DEBUG").unwrap().parse().unwrap()
+}
 
 fn main() {
+    let debug: bool = is_debug();
+
     println!("cargo:rustc-env=PACWRAP_DIST_REPO={}", dist_repo());
     println!("cargo:rustc-env=PACWRAP_BUILDSTAMP={}", head());
-    println!("cargo:rustc-env=PACWRAP_BUILDTIME={}", time());
-    println!("cargo:rustc-env=PACWRAP_BUILD={}", release());
+    println!("cargo:rustc-env=PACWRAP_BUILDTIME={}", time(debug));
+    println!("cargo:rustc-env=PACWRAP_BUILD={}", release(debug));
 }
