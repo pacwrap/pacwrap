0da7635e4b4f5348519c58bdeed45dac272d2610
Xavier <sapphirus@azorium.net>
2023-11-08 15:54:58 -0500

    Some refactoring and formatting

---
+++ pacwrap/src/remove.rs
@@ -0,0 +1,101 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+
+use pacwrap_lib::{log::Logger,
+    sync::transaction::TransactionType,
+    utils::{arguments::Operand, print_help_error},
+    utils::arguments::Arguments,
+    config::cache::InstanceCache,
+    sync::transaction::{TransactionFlags, TransactionAggregator}};
+
+pub fn remove(mut args: &mut Arguments) {
+    let mut cache: InstanceCache = InstanceCache::new();
+
+    let mut logger = Logger::new("pacwrap-sync").init().unwrap();
+    let action = {
+        let mut recursive = 0;
+        let mut cascade = false;
+
+        while let Some(arg) = args.next() {
+            match arg {
+                Operand::Short('s') | Operand::Long("recursive") => recursive += 1,
+                Operand::Short('c') | Operand::Long("cascade") => cascade = true,
+                _ => continue,
+            }
+        }
+
+        TransactionType::Remove(recursive > 0 , cascade, recursive > 1) 
+    };
+    
+    match ascertain_aggregator(action, &mut args, &mut cache, &mut logger) {
+        Ok(ag) => ag.aggregate(&mut InstanceCache::new()), Err(e) => print_help_error(e),
+    }
+}
+
+fn ascertain_aggregator<'a>(
+    action_type: TransactionType, 
+    args: &'a mut Arguments, 
+    inscache: &'a mut InstanceCache, 
+    log: &'a mut Logger) -> Result<TransactionAggregator<'a>, String> { 
+    let mut action_flags = TransactionFlags::NONE;
+    let mut targets = Vec::new();
+    let mut queue: HashMap<Rc<str>,Vec<Rc<str>>> = HashMap::new();
+    let mut current_target = "";
+
+    args.set_index(1);
+
+    if let Operand::None = args.next().unwrap_or_default() {
+        Err("Operation not specified.")?
+    }
+
+    while let Some(arg) = args.next() {
+        match arg {
+            Operand::Long("remove")
+                | Operand::Long("cascade") 
+                | Operand::Long("recursive") 
+                | Operand::Short('R')
+                | Operand::Short('c')  
+                | Operand::Short('s') 
+                | Operand::Short('t') => continue,  
+            Operand::Long("noconfirm") 
+                => action_flags = action_flags | TransactionFlags::NO_CONFIRM,                  
+            Operand::Short('p') 
+                | Operand::Long("preview") 
+                => action_flags = action_flags | TransactionFlags::PREVIEW, 
+            Operand::Long("db-only") 
+                => action_flags = action_flags | TransactionFlags::DATABASE_ONLY,
+            Operand::Long("force-foreign") 
+                => action_flags = action_flags | TransactionFlags::FORCE_DATABASE,
+            Operand::Short('f') 
+                | Operand::Long("filesystem") 
+                => action_flags = action_flags | TransactionFlags::FILESYSTEM_SYNC, 
+            Operand::ShortPos('t', target) 
+                | Operand::LongPos("target", target) 
+                | Operand::ShortPos(_, target) => {
+                current_target = target;
+                targets.push(target.into());
+            },
+            Operand::Value(package) => if current_target != "" {
+                match queue.get_mut(current_target.into()) {
+                    Some(vec) => vec.push(package.into()),
+                    None => { queue.insert(current_target.into(), vec!(package.into())); },
+                }
+            },
+            _ => Err(args.invalid_operand())?,
+        }
+    }
+        
+    if current_target == "" {
+        Err("Target not specified")?
+    }
+
+    let current_target = Some(current_target);
+
+    if targets.len() > 0 {
+        inscache.populate_from(&targets, true);
+    } else {
+        inscache.populate();
+    }
+
+    Ok(TransactionAggregator::new(inscache, queue, log, action_flags, action_type, current_target))
+}
--- pacwrap/src/sync.rs
+++ pacwrap/src/sync.rs
@@ -1,23 +1,20 @@
-use std::collections::HashMap;
-use std::fs::File;
+use std::{collections::HashMap, fs::File};
+use std::rc::Rc;
 use std::io::Write;
 use std::process::exit;
-use std::rc::Rc;
 
-use alpm::{Alpm, PackageReason};
-
-use pacwrap_lib::sync::transaction::{TransactionFlags, TransactionAggregator};
-use pacwrap_lib::{config::{Instance, InstanceType, self},
-    constants::{BAR_GREEN, RESET, BOLD, ARROW_GREEN, BOLD_GREEN},
-    log::Logger,
+use pacwrap_lib::{log::Logger,
     sync::transaction::TransactionType,
-    utils::arguments::Operand,
-    utils::{arguments::Arguments, 
-    print_error,
-    print_help_error},
-config::{InsVars,
-    InstanceHandle,
-    cache::InstanceCache}};
+    utils::{arguments::Operand, print_help_error, print_error},
+    utils::arguments::Arguments,
+    config::{self, 
+        InstanceType, 
+        InsVars, 
+        Instance, 
+        InstanceHandle,
+        cache::InstanceCache},
+    sync::transaction::{TransactionFlags, TransactionAggregator}, 
+    constants::{BAR_GREEN, BOLD, RESET, ARROW_GREEN}};
 
 pub fn synchronize(mut args: &mut Arguments) {
     let mut cache = InstanceCache::new();
@@ -37,7 +34,7 @@ pub fn synchronize(mut args: &mut Arguments) {
         TransactionType::Upgrade(u > 0, y > 0, y > 1)
     };
 
-    match create_type(&mut args) {
+    match create(&mut args) {
         Ok(option) => if let Some(instype) = option {
             if let TransactionType::Upgrade(upgrade, refresh, _) = action { 
                 if ! upgrade {
@@ -47,40 +44,17 @@ pub fn synchronize(mut args: &mut Arguments) {
                 }
             }
 
-            create(instype, args.targets());
+            instantiate(instype, args.targets());
         },
         Err(error) => print_help_error(error),
     }
 
-    match upgrade(action, &mut args, &mut cache, &mut logger) {
+    match ascertain_aggregator(action, &mut args, &mut cache, &mut logger) {
         Ok(ag) => ag.aggregate(&mut InstanceCache::new()), Err(e) => print_help_error(e)
     }
 }
 
-pub fn remove(mut args: &mut Arguments) {
-    let mut cache: InstanceCache = InstanceCache::new();
-    let mut logger = Logger::new("pacwrap-sync").init().unwrap();
-    let action = {
-        let mut recursive = 0;
-        let mut cascade = false;
-
-        while let Some(arg) = args.next() {
-            match arg {
-                Operand::Short('s') | Operand::Long("recursive") => recursive += 1,
-                Operand::Short('c') | Operand::Long("cascade") => cascade = true,
-                _ => continue,
-            }
-        }
-
-        TransactionType::Remove(recursive > 0 , cascade, recursive > 1) 
-    };
-    
-    match remove_ag(action, &mut args, &mut cache, &mut logger) {
-        Ok(ag) => ag.aggregate(&mut InstanceCache::new()), Err(e) => print_help_error(e),
-    }
-}
-
-fn create_type<'a>(args: &mut Arguments) -> Result<Option<InstanceType>, &'a str> {
+fn create<'a>(args: &mut Arguments) -> Result<Option<InstanceType>, &'a str> {
     let mut instype = None;
     let mut create = false;
 
@@ -113,7 +87,7 @@ fn create_type<'a>(args: &mut Arguments) -> Result<Option<InstanceType>, &'a str
     }
 }
 
-pub fn create(instype: InstanceType, mut targets: Vec<&str>) {
+fn instantiate(instype: InstanceType, mut targets: Vec<&str>) {
     if targets.len() == 0 {
         print_help_error("Creation target not specified.");
     }
@@ -184,106 +158,11 @@ fn instantiate_container(ins: &str, deps: Vec<&str>, instype: InstanceType) {
     println!("{} Instantiation complete.", *ARROW_GREEN);
 }
 
-pub fn query(arguments: &mut Arguments) {
-    let mut target = "";
-    let mut explicit = false;
-    let mut quiet = false;
-
-    while let Some(arg) = arguments.next() {
-        match arg {
-            Operand::Short('e') | Operand::Long("explicit") => explicit = true,
-            Operand::Short('q') | Operand::Long("quiet") => quiet = true,
-            Operand::LongPos("target", t) | Operand::ShortPos(_, t) => target = t,
-            _ => print_help_error(arguments.invalid_operand()),
-        }
-    }
-
-    if target.is_empty() {
-        print_help_error("Target not specified.");
-    }
-
-    match config::provide_handle(target) {
-        Ok(handle) => {
-            let root = handle.vars().root().as_ref(); 
-            let handle = Alpm::new2(root, &format!("{}/var/lib/pacman/", root)).unwrap();
-
-            for pkg in handle.localdb().pkgs() {
-                if explicit && pkg.reason() != PackageReason::Explicit {
-                    continue;
-                }
-        
-
-                match quiet {
-                    true => println!("{} ", pkg.name()),
-                    false => println!("{} {}{}{} ", pkg.name(), *BOLD_GREEN, pkg.version(), *RESET), 
-                } 
-            }
-        },
-        Err(error) => {
-            print_error(error);
-            exit(1);
-        }
-    }
-}
-
-pub fn remove_ag<'a>(action_type: TransactionType, args: &'a mut Arguments, inscache: &'a mut InstanceCache, log: &'a mut Logger) -> Result<TransactionAggregator<'a>, String> { 
-    let mut action_flags = TransactionFlags::NONE;
-    let mut targets = Vec::new();
-    let mut queue: HashMap<Rc<str>,Vec<Rc<str>>> = HashMap::new();
-    let mut current_target = "";
-
-    args.set_index(1);
-
-    if let Operand::None = args.next().unwrap_or_default() {
-        Err("Operation not specified.")?
-    }
-
-    while let Some(arg) = args.next() {
-        match arg {
-            Operand::Long("remove")
-                | Operand::Long("cascade") 
-                | Operand::Long("recursive") 
-                | Operand::Short('R')
-                | Operand::Short('c')  
-                | Operand::Short('s') 
-                | Operand::Short('t') => continue,  
-            Operand::Long("noconfirm") | Operand::Long("no-confirm") => action_flags = action_flags | TransactionFlags::NO_CONFIRM,                  
-            Operand::Short('p') | Operand::Long("preview") => action_flags = action_flags | TransactionFlags::PREVIEW, 
-            Operand::Long("db-only") => action_flags = action_flags | TransactionFlags::DATABASE_ONLY,
-            Operand::Long("force-foreign") => action_flags = action_flags | TransactionFlags::FORCE_DATABASE,
-            Operand::Short('f') | Operand::Long("filesystem") => action_flags = action_flags | TransactionFlags::FILESYSTEM_SYNC, 
-            Operand::ShortPos('t', target) 
-                | Operand::LongPos("target", target) 
-                | Operand::ShortPos(_, target) => {
-                current_target = target;
-                targets.push(target.into());
-            },
-            Operand::Value(package) => if current_target != "" {
-                match queue.get_mut(current_target.into()) {
-                    Some(vec) => vec.push(package.into()),
-                    None => { queue.insert(current_target.into(), vec!(package.into())); },
-                }
-            },
-            _ => Err(args.invalid_operand())?,
-        }
-    }
-        
-    if current_target == "" {
-        Err("Target not specified")?
-    }
-
-    let current_target = Some(current_target);
-
-    if targets.len() > 0 {
-        inscache.populate_from(&targets, true);
-    } else {
-        inscache.populate();
-    }
-
-    Ok(TransactionAggregator::new(inscache, queue, log, action_flags, action_type, current_target))
-}
-
-pub fn upgrade<'a>(action_type: TransactionType, args: &'a mut Arguments, inscache: &'a mut InstanceCache, log: &'a mut Logger) -> Result<TransactionAggregator<'a>, String> { 
+fn ascertain_aggregator<'a>(
+    action_type: TransactionType, 
+    args: &'a mut Arguments, 
+    inscache: &'a mut InstanceCache, 
+    log: &'a mut Logger) -> Result<TransactionAggregator<'a>, String> { 
     let mut action_flags = TransactionFlags::NONE;
     let mut targets = Vec::new();
     let mut queue: HashMap<Rc<str>,Vec<Rc<str>>> = HashMap::new();
@@ -301,14 +180,26 @@ pub fn upgrade<'a>(action_type: TransactionType, args: &'a mut Arguments, inscac
                 | Operand::Short('t') | Operand::Long("target") 
                 | Operand::Short('y') | Operand::Long("refresh")
                 | Operand::Short('u') | Operand::Long("upgrade") => continue,
-            Operand::Short('o') | Operand::Long("target-only") => action_flags = action_flags | TransactionFlags::TARGET_ONLY,
-            Operand::Short('f') | Operand::Long("filesystem") => action_flags = action_flags | TransactionFlags::FILESYSTEM_SYNC, 
-            Operand::Short('p') | Operand::Long("preview") => action_flags = action_flags | TransactionFlags::PREVIEW,
-            Operand::Short('c') | Operand::Long("create") => action_flags = action_flags | TransactionFlags::CREATE | TransactionFlags::FORCE_DATABASE,
+            Operand::Short('o') 
+                | Operand::Long("target-only") 
+                => action_flags = action_flags | TransactionFlags::TARGET_ONLY,
+            Operand::Short('f') 
+                | Operand::Long("filesystem") 
+                => action_flags = action_flags | TransactionFlags::FILESYSTEM_SYNC, 
+            Operand::Short('p') 
+                | Operand::Long("preview") 
+                => action_flags = action_flags | TransactionFlags::PREVIEW,
+            Operand::Short('c') 
+                | Operand::Long("create") 
+                => action_flags = action_flags | TransactionFlags::CREATE 
+                    | TransactionFlags::FORCE_DATABASE,
             Operand::Short('b') | Operand::Long("base") => base = true,
-            Operand::Long("db-only") => action_flags = action_flags | TransactionFlags::DATABASE_ONLY,
-            Operand::Long("force-foreign") => action_flags = action_flags | TransactionFlags::FORCE_DATABASE,
-            Operand::Long("noconfirm") => action_flags = action_flags | TransactionFlags::NO_CONFIRM, 
+            Operand::Long("db-only") 
+                => action_flags = action_flags | TransactionFlags::DATABASE_ONLY,
+            Operand::Long("force-foreign") 
+                => action_flags = action_flags | TransactionFlags::FORCE_DATABASE,
+            Operand::Long("noconfirm") 
+                => action_flags = action_flags | TransactionFlags::NO_CONFIRM, 
             Operand::ShortPos('t', target) 
                 | Operand::LongPos("target", target) => {
                 current_target = target;
