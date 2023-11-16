3cff47783075f757b817ddf0c21fdb7e633a0508
Xavier <sapphirus@azorium.net>
2023-11-16 01:03:53 -0500

    New and updated dist scripts, dist package, and build-environment configuration
    - makepkg.conf for building dist-package in build chroot
    - runtime.sh to package a lightweight runtime image for pacwrap-agent
    - Link to flatpak-xdg-utils binaries to /usr/local/bin in
      pacwrap-base-dist package.

--- dist/tools/clean.sh
+++ dist/tools/clean.sh
@@ -1,2 +1,51 @@
-rm ./dist/dist-repo/*
-rm ./dist/pacwrap-base-dist/*.tar.zst
+#!/bin/bash
+#
+#  pacwrap - clean.sh
+# 
+#  Copyright (C) 2023 Xavier R.M. 
+#  sapphirus(at)azorium(dot)net
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, with only version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+BOLD=$(tput bold)
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+RESET=$(tput sgr0)
+DIST_RUNTIME="./dist/runtime"
+DIST_BASE="./dist/pacwrap-base-dist"
+DIST_REPO="./dist/dist-repo"
+
+runtime() {
+	if [[ -d "$DIST_RUNTIME" ]]; then
+		rm -r "$DIST_RUNTIME"
+		echo "$BOLD$GREEN     Cleaned$RESET container runtime"
+	fi
+}
+
+repo() {
+	if [[ -d "$DIST_REPO" ]] && [[ -d "$DIST_BASE" ]]; then
+		rm $DIST_REPO/*  $DIST_BASE/*.tar.zst
+		echo "$BOLD$GREEN     Cleaned$RESET repositories"
+	fi
+}
+
+invalid() {
+	echo $BOLD$RED"error:$RESET Invalid parameter '$1'"
+}
+
+for var in "$@"; do case $var in 
+	repo) repo;;
+	runtime) runtime;;
+	*) invalid $var;;
+esac; done
