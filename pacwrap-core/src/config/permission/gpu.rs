3e30b9a4c65d5f2fb9fb8b2e9cf35af4498182bd
Xavier <sapphirus@azorium.net>
2024-01-03 18:21:04 -0500

    Inclusion of GPL headers to declare copyright and licensing
    - LICENSE has been included since the incarnation of this project,
      therefore nothing changes except providing prominent notice.
    - Cleaned up distribution scripting, with inclusion of updated headers

--- pacwrap-core/src/config/permission/gpu.rs
+++ pacwrap-core/src/config/permission/gpu.rs
@@ -1,3 +1,22 @@
+/*
+ * pacwrap-core
+ * 
+ * Copyright (C) 2023-2024 Xavier R.M. <sapphirus@azorium.net>
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::fs::read_dir;
 use std::path::Path;
 
@@ -53,5 +72,6 @@ fn populate_dev() -> Vec<String> {
             }
         }
     }
+
     vec
 }
--- pacwrap-core/src/sync/resolver.rs
+++ pacwrap-core/src/sync/resolver.rs
@@ -1,3 +1,22 @@
+/*
+ * pacwrap-core
+ * 
+ * Copyright (C) 2023-2024 Xavier R.M. <sapphirus@azorium.net>
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::collections::HashSet;
 
 use alpm::{Package, Alpm};
