7ed366f809f350e40275a7122f3689b7d97f105d
Xavier <sapphirus@azorium.net>
2024-01-03 18:21:04 -0500

    Inclusion of GPL headers to declare copyright and licensing
    - LICENSE has been included since the incarnation of this project,
      therefore nothing changes except providing prominent notice.
    - Cleaned up distribution scripting, with inclusion of updated headers

--- pacwrap-core/src/sync/resolver_local.rs
+++ pacwrap-core/src/sync/resolver_local.rs
@@ -1,10 +1,28 @@
+/*
+ * pacwrap-core
+ * 
+ * Copyright (C) 2023-2024 Xavier R.M. <sapphirus@azorium.net>
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::collections::HashSet;
 
 use alpm::{Package, Alpm, PackageReason};
 
 use super::{transaction::{ErrorKind, TransactionType}, utils::AlpmUtils};
 
-#[allow(dead_code)]
 pub struct LocalDependencyResolver<'a> {
     resolved: HashSet<&'a str>,
     packages: Vec<Package<'a>>,
