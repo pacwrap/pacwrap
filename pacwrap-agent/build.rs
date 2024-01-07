c81bc26c6c36b6659cee6aabb399e58c9a556b3c
Xavier <sapphirus@azorium.net>
2024-01-07 02:37:59 -0500

    Panic if build is attempted on unsupported operating environment

--- pacwrap-agent/build.rs
+++ pacwrap-agent/build.rs
@@ -1,10 +1,10 @@
 /*
- * pacwrap-core
+ * pacwrap-agent
  * 
  * Copyright (C) 2023-2024 Xavier R.M. <sapphirus@azorium.net>
  * SPDX-License-Identifier: GPL-3.0-only
  *
- * This library is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3.
  *
@@ -17,20 +17,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-use std::env::var;
-
-fn dist_repo() -> String {
-    match var("PACWRAP_DIST_REPO") {
-        Ok(var) => var,
-        Err(_) => "/usr/share/pacwrap/repo".into(),
-    }
-}
-
 fn main() {
     if ! cfg!(target_os="linux") || ! cfg!(target_family="unix") {
         panic!("Unsupported build target. Please refer to the documentation for further information.")
     }
-
-    println!("cargo:rerun-if-env-changed=PACWRAP_DIST_REPO");
-    println!("cargo:rustc-env=PACWRAP_DIST_REPO={}", dist_repo());
 }
