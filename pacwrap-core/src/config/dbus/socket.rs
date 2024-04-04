5ed6240ffeee2dd835af9647e9cfaa25a287c3e9
Xavier <sapphirus@azorium.net>
2024-04-03 21:51:43 -0400

    Changed `socket` string to `policy` to better reflect what it does

--- pacwrap-core/src/config/dbus/socket.rs
+++ pacwrap-core/src/config/dbus/socket.rs
@@ -4,19 +4,19 @@ use crate::{config::Dbus, exec::args::ExecutionArgs};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Socket {
-    socket: String,
+    policy: String,
     address: Vec<String>,
 }
 
 #[typetag::serde(name = "socket")]
 impl Dbus for Socket {
     fn register(&self, args: &mut ExecutionArgs) {
-        match self.socket.to_lowercase().as_str() {
+        match self.policy.to_lowercase().as_str() {
             p if p == "call" || p == "talk" || p == "see" || p == "own" || p == "broadcast" =>
                 for sock in self.address.iter() {
                     args.dbus(p, sock);
                 },
-            &_ => {}
+            _ => {}
         }
     }
 }
